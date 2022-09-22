ce4c510b8b9d0d3dc3c5e0189f51587b075beaf1
Kron1749 <drakgr98@gmail.com>
2022-09-22 17:24:33 +0300

    Staging test created

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
@@ -28,17 +28,9 @@ module.exports = {
             chainId: 5,
             blockConfirmations: 6,
         },
-        rinkeby: {
-            url: RINKEBY_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            saveDeployments: true,
-            chainId: 4,
-            blockConfirmations: 6,
-        },
     },
     etherscan: {
         apiKey: {
-            rinkeby: ETHERSCAN_API_KEY,
             goerli: ETHERSCAN_API_KEY,
         },
     },
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,15 +6,6 @@ const networkConfig = {
         mintFee: "10000000000000000", // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
     },
-
-    4: {
-        name: "rinkeby",
-        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
-        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        callbackGasLimit: "500000", // 500,000 gas
-        mintFee: "10000000000000000", // 0.01 ETH
-    },
     5: {
         name: "goerli",
         ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
