0dcc1892ff6e4a9e4db9b2721b0a992c82e17cd8
Kron1749 <70746258+Kron1749@users.noreply.github.com>
2022-09-23 15:20:08 +0300

    Created script for frontend update

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -4,11 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestToken is ERC20,Ownable {
-
+contract TestToken is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("TestToke", "TT") {
         _mint(msg.sender, initialSupply);
     }
-
-    
 }
---
+++ deploy/99-update-frontend.js
@@ -0,0 +1,75 @@
+const { ethers, network } = require("hardhat")
+const fs = require("fs")
+const FRONT_END_TEST_TOKEN_ADDRESSES =
+    "../full-defi-app-hardhat-frontend/Constants/TestToken/contractAddressesTestToken.json"
+const FRONT_END_TEST_TOKEN_ABI_FILE =
+    "../full-defi-app-hardhat-frontend/Constants/TestToken/abiTestToken.json"
+const FRONT_END_STAKING_REWARDS_ADDRESSES =
+    "../full-defi-app-hardhat-frontend/Constants/StakingRewards/contractAddressesStakingRewards.json"
+const FRONT_END_STAKING_REWARDS_ABI_FILE =
+    "../full-defi-app-hardhat-frontend/Constants/StakingRewards/abiStakingRewards.json"
+
+module.exports = async function () {
+    if (process.env.UPDATE_FRONT_END) {
+        console.log("Updating front end")
+        updateContractAddressesTestToken()
+        updateAbiTestToken()
+        updateContractAddressesStakingRewards()
+        updateABIStakingRewards()
+    }
+}
+
+async function updateContractAddressesTestToken() {
+    const testToken = await ethers.getContract("TestToken")
+    const chainId = network.config.chainId.toString()
+    const currentTestTokenAddresses = JSON.parse(
+        fs.readFileSync(FRONT_END_TEST_TOKEN_ADDRESSES, "utf8")
+    )
+    if (chainId in currentTestTokenAddresses) {
+        if (!currentTestTokenAddresses[chainId].includes(testToken.address)) {
+            currentTestTokenAddresses[chainId].push(testToken.address)
+        }
+    }
+    {
+        currentTestTokenAddresses[chainId] = [testToken.address]
+    }
+    fs.writeFileSync(FRONT_END_TEST_TOKEN_ADDRESSES, JSON.stringify(currentTestTokenAddresses))
+}
+
+async function updateAbiTestToken() {
+    const testToken = await ethers.getContract("TestToken")
+    fs.writeFileSync(
+        FRONT_END_TEST_TOKEN_ABI_FILE,
+        testToken.interface.format(ethers.utils.FormatTypes.json)
+    )
+}
+
+async function updateContractAddressesStakingRewards() {
+    const stakingRewards = await ethers.getContract("StakingRewards")
+    const chainId = network.config.chainId.toString()
+    const currentStakingRewardsAddresses = JSON.parse(
+        fs.readFileSync(FRONT_END_STAKING_REWARDS_ADDRESSES, "utf8")
+    )
+    if (chainId in currentStakingRewardsAddresses) {
+        if (!currentStakingRewardsAddresses[chainId].includes(stakingRewards.address)) {
+            currentStakingRewardsAddresses[chainId].push(stakingRewards.address)
+        }
+    }
+    {
+        currentStakingRewardsAddresses[chainId] = [stakingRewards.address]
+    }
+    fs.writeFileSync(
+        FRONT_END_STAKING_REWARDS_ADDRESSES,
+        JSON.stringify(currentStakingRewardsAddresses)
+    )
+}
+
+async function updateABIStakingRewards() {
+    const stakingRewards = await ethers.getContract("StakingRewards")
+    fs.writeFileSync(
+        FRONT_END_STAKING_REWARDS_ABI_FILE,
+        stakingRewards.interface.format(ethers.utils.FormatTypes.json)
+    )
+}
+
+module.exports.tags = ["all", "frontend"]
