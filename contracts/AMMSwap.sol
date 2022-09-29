5d5e88827fd5ed9f65b13faa5d395f2978daf4c0
Kron1749 <70746258+Kron1749@users.noreply.github.com>
2022-09-29 23:28:31 +0300

    AMMSwap created tests for swap function

--- contracts/AMMSwap.sol
+++ contracts/AMMSwap.sol
@@ -1,183 +1,202 @@
 /**
-    1. Add 2 tokens what to swap
-    2. Create swap function
-    3. Create add liquadity function
-    4. Create remove liquadity function
+    Need to update fee transfer
 
  */
 
- // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-error AMMSWAP__InvalidAddress();
-error AMMSWAP_AmountIsLessThanZero();
-error AMMSWAP_NotEnoughAllowance();
-error AMMSWAP_PriceIsNotEqual();
-error AMMSWAP_SharesIsZero();
-error Amount0OrAmount1IsLessOrZero();
+error AMMSwap__InvalidAddress();
+error AMMSwap__AmountIsLessThanZero();
+error AMMSwap__NotEnoughAllowance();
+error AMMSwap__PriceIsNotEqual();
+error AMMSwap__SharesIsZero();
+error AMMSwap__Amount0OrAmount1IsLessOrZero();
+error AMMSwap__NotEnoughBalanceOfContract();
+error AMMSwap__NotEnoughBalanceOfAccout();
+error AMMSwap__NotOwner();
 
 contract AMMSwap {
-
-    event TokensSwaped(address indexed token0,address indexed token1,uint256 amountIn,uint256 amountOut);
-    event LiquidityAdded(uint256 indexed amount0,uint256 indexed amount1,uint256 indexed shares);
-    event LiquidityRemoved(uint256 indexed shares,uint256 indexed amount0,uint256 indexed amount1);
+    event TokensSwaped(
+        address indexed token0,
+        address indexed token1,
+        uint256 amountIn,
+        uint256 amountOut
+    );
+    event LiquidityAdded(uint256 indexed amount0, uint256 indexed amount1, uint256 indexed shares);
+    event LiquidityRemoved(
+        uint256 indexed shares,
+        uint256 indexed amount0,
+        uint256 indexed amount1
+    );
 
     IERC20 public immutable i_token0;
-    
+
     IERC20 public immutable i_token1;
 
-    uint private s_balance0;
-    uint private s_balance1;
+    uint256 private s_balance0;
+    uint256 private s_balance1;
 
-    uint private s_totalSupply;
-    mapping(address=>uint256) private s_balanceOf;
+    address private s_owner;
+    uint256 private s_totalSupply;
+    mapping(address => uint256) private s_balanceOf;
 
-    constructor(address _token0,address _token1){
-        i_token0 = IERC20(_token0);
-        i_token1 = IERC20(_token1);
+    modifier onlyOwner() {
+        if (s_owner != msg.sender) {
+            revert AMMSwap__NotOwner();
+        }
+        _;
     }
 
+    constructor(address _token0, address _token1) {
+        i_token0 = IERC20(_token0);
+        i_token1 = IERC20(_token1);
+        s_owner = msg.sender;
+    }
 
-
-    function swap(address _tokenIn,uint256 _amountIn) external returns(uint256 amountOut) {
-        if(_tokenIn != address(i_token0) || _tokenIn != address(i_token1)) {
-            revert AMMSWAP__InvalidAddress();
+    function swap(address _tokenIn, uint256 _amountIn) external returns (uint256 amountOut) {
+        if (_tokenIn != address(i_token0) && _tokenIn != address(i_token1)) {
+            revert AMMSwap__InvalidAddress();
         }
-        if(_amountIn<=0) {
-            revert AMMSWAP_AmountIsLessThanZero();
+        if (_amountIn <= 0) {
+            revert AMMSwap__AmountIsLessThanZero();
         }
         bool isToken0 = _tokenIn == address(i_token0);
 
         // Get what tokens are
-        (IERC20 tokenIn,IERC20 tokenOut,uint256 balanceIn,uint256 balanceOut) = isToken0 ? (i_token0,i_token1,s_balance0,s_balance1) : (i_token1,i_token0,s_balance1,s_balance0);
+        (IERC20 tokenIn, IERC20 tokenOut, uint256 balanceIn, uint256 balanceOut) = isToken0
+            ? (i_token0, i_token1, s_balance0, s_balance1)
+            : (i_token1, i_token0, s_balance1, s_balance0);
 
-        if(tokenIn.allowance(msg.sender,address(this))<_amountIn) {
-            revert AMMSWAP_NotEnoughAllowance();
+        if (tokenIn.allowance(msg.sender, address(this)) < _amountIn) {
+            revert AMMSwap__NotEnoughAllowance();
         }
-        tokenIn.transferFrom(msg.sender,address(this),_amountIn);
+        if (tokenIn.balanceOf(msg.sender) < _amountIn) {
+            revert AMMSwap__NotEnoughBalanceOfAccout();
+        }
+        tokenIn.transferFrom(msg.sender, address(this), _amountIn);
 
-        uint256 amountInWithFee = (_amountIn * 997)/1000;
+        uint256 amountInWithFee = (_amountIn * 997) / 1000;
         // By formula ydx/(x+dx)=dy - amountOut
-        amountOut = (balanceOut*amountInWithFee)/(balanceIn+amountInWithFee);
+        amountOut = (balanceOut * amountInWithFee) / (balanceIn + amountInWithFee);
         //Transfer token out to sender
-        tokenOut.transfer(msg.sender,amountOut);
+        if (tokenOut.balanceOf(address(this)) < amountOut) {
+            revert AMMSwap__NotEnoughBalanceOfContract();
+        }
+        tokenOut.transfer(msg.sender, amountOut);
 
         // Update balances
-        _updateBalances(
-            i_token0.balanceOf(address(this)),
-            i_token1.balanceOf(address(this))
-        );
+        _updateBalances(i_token0.balanceOf(address(this)), i_token1.balanceOf(address(this)));
         emit TokensSwaped(address(tokenIn), address(tokenOut), _amountIn, amountOut);
-
     }
 
-    function addLiquidity(uint256 _amount0,uint256 _amount1) external returns(uint256 shares) {
+    function addLiquidity(uint256 _amount0, uint256 _amount1) external returns (uint256 shares) {
         // Send token0 and token1
-        if(i_token0.allowance(msg.sender, address(this))<=_amount0 || i_token1.allowance(msg.sender, address(this))<=_amount1) {
-            revert AMMSWAP_NotEnoughAllowance();
+        if (
+            i_token0.allowance(msg.sender, address(this)) < _amount0 ||
+            i_token1.allowance(msg.sender, address(this)) < _amount1
+        ) {
+            revert AMMSwap__NotEnoughAllowance();
         }
         i_token0.transferFrom(msg.sender, address(this), _amount0);
-        i_token1.transferFrom(msg.sender,address(this), _amount1);
+        i_token1.transferFrom(msg.sender, address(this), _amount1);
 
         // dy/dx = X/Y this formula must be right
 
-        if(s_balance0>0 || s_balance1 >0 ) {
-            if(s_balance0*_amount1!=s_balance1*_amount0) {
-                revert AMMSWAP_PriceIsNotEqual();
+        if (s_balance0 > 0 || s_balance1 > 0) {
+            if (s_balance0 * _amount1 != s_balance1 * _amount0) {
+                revert AMMSwap__PriceIsNotEqual();
             }
         }
- 
+
         // Mint shares
         // f(x,y) = sqrt(XY) value of liquadity
         // s = dx/x*T=dy/y*T
-        if(s_totalSupply==0) {
-            shares = _sqrt(_amount0*_amount1);
+        if (s_totalSupply == 0) {
+            shares = _sqrt(_amount0 * _amount1);
         } else {
             shares = _min(
-                (_amount0*s_totalSupply)/s_balance0,
-                (_amount1*s_totalSupply)/s_balance1
+                (_amount0 * s_totalSupply) / s_balance0,
+                (_amount1 * s_totalSupply) / s_balance1
             );
         }
-        if(shares<=0) {
-            revert AMMSWAP_SharesIsZero();
+        if (shares <= 0) {
+            revert AMMSwap__SharesIsZero();
         }
-        _mint(msg.sender,shares);
+        _mint(msg.sender, shares);
 
         // Update balances
         _updateBalances(i_token0.balanceOf(address(this)), i_token1.balanceOf(address(this)));
         emit LiquidityAdded(_amount0, _amount1, shares);
     }
 
-    function removeLiquidity(uint256 _shares) external  returns(uint256 amount0,uint256 amount1){
+    function removeLiquidity(uint256 _shares) external returns (uint256 amount0, uint256 amount1) {
         // calculate amount0 and amount1 to withdraw
         // dx = s/T*Y
         // dy = s/T*Y
         uint256 bal0 = i_token0.balanceOf(address(this));
         uint256 bal1 = i_token1.balanceOf(address(this));
 
-        amount0 = (_shares*bal0)/s_totalSupply;
-        amount1 = (_shares*bal1)/s_totalSupply;
-        if(amount0<=0 || amount1<=0) {
-            revert Amount0OrAmount1IsLessOrZero();
+        amount0 = (_shares * bal0) / s_totalSupply;
+        amount1 = (_shares * bal1) / s_totalSupply;
+        if (amount0 <= 0 || amount1 <= 0) {
+            revert AMMSwap__Amount0OrAmount1IsLessOrZero();
         }
         // Burn shares
         _burn(msg.sender, _shares);
         // Update balances
-        _updateBalances(bal0-amount0, bal1-amount1);
+        _updateBalances(bal0 - amount0, bal1 - amount1);
         // Transfer tokens to msg.sender
         i_token0.transfer(msg.sender, amount0);
         i_token1.transfer(msg.sender, amount1);
         emit LiquidityRemoved(_shares, amount0, amount1);
     }
 
-    function getTotalSupply() public view returns(uint256) {
-        return s_totalSupply;
-    }
-
-    function getBalanceOfToken(address _user) public view returns(uint256) {
-        return s_balanceOf[_user];
-    }
-
-    function getBalanceOfToken0() public view returns(uint256) {
-        return s_balance0;
-    }
-
-    function getBalanceOfToken1()public view returns(uint256) {
-        return s_balance1;
-    }
-
-    function getAddressToken0() public view returns(address) {
-        return address(i_token0);
-    } 
-
-    function getAddressToken1() public view returns(address) {
-        return address(i_token1);
-    }
-
-
-    function _mint(address _to,uint _amount) private {
-        s_balanceOf[_to] += _amount;
-        s_totalSupply += _amount;
-    }
-
-    function _burn(address _from,uint _amount) private {
-        s_balanceOf[_from] -= _amount;
-        s_totalSupply -= _amount;
-    }
-
-    function _updateBalances(uint256 _reserve0,uint256 _reserve1) private {
+    function _updateBalances(uint256 _reserve0, uint256 _reserve1) public onlyOwner {
         s_balance0 = _reserve0;
         s_balance1 = _reserve1;
     }
 
-    
+    function getTotalSupply() public view returns (uint256) {
+        return s_totalSupply;
+    }
 
-    // Got from uniswap 
-    function _sqrt(uint y) private pure returns (uint z) {
+    function getBalanceOfShares(address _user) public view returns (uint256) {
+        return s_balanceOf[_user];
+    }
+
+    function getBalanceOfToken0() public view returns (uint256) {
+        return s_balance0;
+    }
+
+    function getBalanceOfToken1() public view returns (uint256) {
+        return s_balance1;
+    }
+
+    function getAddressToken0() public view returns (address) {
+        return address(i_token0);
+    }
+
+    function getAddressToken1() public view returns (address) {
+        return address(i_token1);
+    }
+
+    function _mint(address _to, uint256 _amount) private {
+        s_balanceOf[_to] += _amount;
+        s_totalSupply += _amount;
+    }
+
+    function _burn(address _from, uint256 _amount) private {
+        s_balanceOf[_from] -= _amount;
+        s_totalSupply -= _amount;
+    }
+
+    // Got from uniswap
+    function _sqrt(uint256 y) private pure returns (uint256 z) {
         if (y > 3) {
             z = y;
-            uint x = y / 2 + 1;
+            uint256 x = y / 2 + 1;
             while (x < z) {
                 z = x;
                 x = (y / x + x) / 2;
@@ -187,7 +206,7 @@ contract AMMSwap {
         }
     }
 
-    function _min(uint x, uint y) private pure returns (uint) {
+    function _min(uint256 x, uint256 y) private pure returns (uint256) {
         return x <= y ? x : y;
     }
 }
--- test/unit/AMMSwap.test.js
+++ test/unit/AMMSwap.test.js
@@ -2,61 +2,154 @@ const { assert, expect } = require("chai")
 const { network, deployments, ethers } = require("hardhat")
 const { developmentChains } = require("../../helper-hardhat-config")
 
-!developmentChains.includes(network.name)     ? describe.skip
-: describe("AMMSwap Tests", function () {
-    let testToken0,testToken1,AMMSwap,deployer
-    beforeEach(async function(){
-        accounts = await ethers.getSigners()
-        deployer = accounts[0]
-        await deployments.fixture(["all"])
-        testToken0 = await ethers.getContract("TestToken")
-        testToken1 = await ethers.getContract("TestToken1")
-        AMMSwap = await ethers.getContract("AMMSwap")
-    })
-    describe("Constructor",function(){
-        it("Should properyl set tokens addresses",async function(){
-            const token0 = await AMMSwap.getAddressToken0()
-            const token1 = await AMMSwap.getAddressToken1()
-            assert.equal(token0,testToken0.address)
-            assert.equal(token1,testToken1.address)
-        })
-    })
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("AMMSwap Tests", function () {
+          let testToken0, testToken1, AMMSwap, deployer
+          const AMOUNT = 100
+          beforeEach(async function () {
+              accounts = await ethers.getSigners()
+              deployer = accounts[0]
+              await deployments.fixture(["all"])
+              testToken0 = await ethers.getContract("TestToken")
+              testToken1 = await ethers.getContract("TestToken1")
+              AMMSwap = await ethers.getContract("AMMSwap")
+          })
+          describe("Constructor", function () {
+              it("Should properyl set tokens addresses", async function () {
+                  const token0 = await AMMSwap.getAddressToken0()
+                  const token1 = await AMMSwap.getAddressToken1()
+                  assert.equal(token0, testToken0.address)
+                  assert.equal(token1, testToken1.address)
+              })
+          })
 
-    describe("Add Liquidity",function() {
-        it("Should not add if allowance for both tokens is too low",async function(){
-            await expect(AMMSwap.addLiquidity(100,100)).to.be.revertedWith("AMMSWAP_NotEnoughAllowance")
-        })
-        it("Should not if allowance for first token is low",async function(){
+          describe("Add Liquidity", function () {
+              it("Should not add if allowance for both tokens is too low", async function () {
+                  await expect(AMMSwap.addLiquidity(AMOUNT, AMOUNT)).to.be.revertedWith(
+                      "AMMSwap__NotEnoughAllowance"
+                  )
+              })
+              it("Should not if allowance for second token is low", async function () {
+                  await testToken0.increaseAllowance(AMMSwap.address, AMOUNT)
+                  await expect(AMMSwap.addLiquidity(AMOUNT, AMOUNT)).to.be.revertedWith(
+                      "AMMSwap__NotEnoughAllowance"
+                  )
+              })
+              it("Should not if allowance for first tokens is low", async function () {
+                  await testToken1.increaseAllowance(AMMSwap.address, AMOUNT)
+                  await expect(AMMSwap.addLiquidity(AMOUNT, AMOUNT)).to.be.revertedWith(
+                      "AMMSwap__NotEnoughAllowance"
+                  )
+              })
+              it("Should not add if shares <=0", async function () {
+                  await testToken0.increaseAllowance(AMMSwap.address, AMOUNT)
+                  await testToken1.increaseAllowance(AMMSwap.address, AMOUNT)
+                  //Adding for first time
+                  await expect(AMMSwap.addLiquidity(AMOUNT, 0)).to.be.revertedWith(
+                      "AMMSwap__SharesIsZero"
+                  )
+              })
+              it("Should not add if formula is not right", async function () {
+                  await testToken0.increaseAllowance(AMMSwap.address, AMOUNT * 2)
+                  await testToken1.increaseAllowance(AMMSwap.address, AMOUNT * 2)
+                  //Adding for first time to update balances
+                  await AMMSwap.addLiquidity(AMOUNT, AMOUNT)
+                  //Adding for first time with different amounts
+                  await expect(AMMSwap.addLiquidity(AMOUNT, AMOUNT - 1)).to.be.revertedWith(
+                      "AMMSwap__PriceIsNotEqual"
+                  )
+              })
+              it("Should properly add liquadity", async function () {
+                  await testToken0.increaseAllowance(AMMSwap.address, AMOUNT)
+                  await testToken1.increaseAllowance(AMMSwap.address, AMOUNT)
+                  expect(await AMMSwap.addLiquidity(AMOUNT, AMOUNT)).to.emit("LiquidityAdded")
+                  const balanceOfToken0FromContract = await AMMSwap.getBalanceOfToken0()
+                  const balanceOfToken0 = await testToken0.balanceOf(AMMSwap.address)
+                  const balanceOfToken1FromContract = await AMMSwap.getBalanceOfToken1()
+                  const balanceOfToken1 = await testToken1.balanceOf(AMMSwap.address)
+                  const shares = await AMMSwap.getBalanceOfShares(deployer.address)
+                  const totalSupply = await AMMSwap.getTotalSupply()
+                  assert.equal(balanceOfToken0FromContract.toString(), balanceOfToken0.toString())
+                  assert.equal(balanceOfToken1FromContract.toString(), balanceOfToken1.toString())
+                  assert.equal(shares.toString(), AMOUNT.toString())
+                  assert.equal(totalSupply.toString(), AMOUNT.toString())
+              })
+          })
+          describe("Swap", function () {
+              it("Should not swap if address is invalid", async function () {
+                  await expect(AMMSwap.swap(deployer.address, 100)).to.be.revertedWith(
+                      "AMMSwap__InvalidAddress"
+                  )
+              })
 
-        })
-        it("Should not if allowance for second tokens is low",async function(){
-            
-        })
-        it("Should not add if formula is not right",async function(){
+              it("Should not swap if amountIn <=0", async function () {
+                  await expect(AMMSwap.swap(testToken0.address, 0)).to.be.revertedWith(
+                      "AMMSwap__AmountIsLessThanZero"
+                  )
+              })
+              it("Should not swap if not enough allowance", async function () {
+                  await expect(AMMSwap.swap(testToken0.address, 100)).to.be.revertedWith(
+                      "AMMSwap__NotEnoughAllowance"
+                  )
+              })
+              it("Should not swap if not enough user tokens ", async function () {
+                  const balanceOfToken0 = await testToken0.balanceOf(deployer.address)
+                  const balanceToStake = balanceOfToken0 + 1
+                  await testToken0.increaseAllowance(AMMSwap.address, balanceToStake)
+                  await expect(
+                      AMMSwap.swap(testToken0.address, balanceToStake)
+                  ).to.be.revertedWith("AMMSwap__NotEnoughBalanceOfAccout")
+              })
+              it("Should not swap if not enough contract tokens", async function () {
+                  await testToken0.increaseAllowance(AMMSwap.address, AMOUNT)
+                  await AMMSwap._updateBalances(0, 10000)
+                  await expect(AMMSwap.swap(testToken0.address, AMOUNT)).to.be.revertedWith(
+                      "AMMSwap__NotEnoughBalanceOfContract"
+                  )
+              })
+              it("Should properly swap tokens", async function () {
+                  await testToken0.increaseAllowance(AMMSwap.address, AMOUNT)
+                  await testToken1.increaseAllowance(AMMSwap.address, AMOUNT)
+                  expect(await AMMSwap.addLiquidity(AMOUNT / 2, AMOUNT / 2)).to.emit(
+                      "LiquidityAdded"
+                  )
+                  const amountOfToken0Before = await testToken0.balanceOf(AMMSwap.address)
+                  const amountOfToken1Before = await testToken1.balanceOf(AMMSwap.address)
+                  expect(await AMMSwap.swap(testToken0.address, AMOUNT / 4)).to.emit(
+                      "TokensSwaped"
+                  )
+                  const amountInWithFee = ((AMOUNT / 4) * 997) / 1000
+                  const balanceOfToken0AfterCalculated =
+                      amountOfToken0Before.toNumber() + AMOUNT / 4
+                  const balanceOfToken1AfterCalculated = Math.ceil(
+                      amountOfToken1Before.toNumber() -
+                          ((AMOUNT / 2) * amountInWithFee) / (AMOUNT / 2 + amountInWithFee)
+                  )
 
-        })
-        it("Should not add if shares <=0",async function() {
+                  const balanceOfToken0FromContractAfter = await AMMSwap.getBalanceOfToken0()
+                  const balanceOfToken0After = await testToken0.balanceOf(AMMSwap.address)
+                  const balanceOfToken1FromContractAfter = await AMMSwap.getBalanceOfToken1()
+                  const balanceOfToken1After = await testToken1.balanceOf(AMMSwap.address)
 
-        })
-        it("Shoudl properly add liquadity",async function(){
+                  assert.equal(
+                      balanceOfToken0FromContractAfter.toString(),
+                      balanceOfToken0After.toString()
+                  )
+                  assert.equal(
+                      balanceOfToken1FromContractAfter.toString(),
+                      balanceOfToken1After.toString()
+                  )
+                  assert.equal(
+                      balanceOfToken0AfterCalculated.toString(),
+                      balanceOfToken0After.toString()
+                  )
+                  assert.equal(
+                      balanceOfToken1AfterCalculated.toString(),
+                      balanceOfToken1After.toString()
+                  )
+              })
+          })
 
-        })
-    })
-    describe("Swap",function(){
-        it("Should not swap if address is illegal",async function(){
-            await expect(AMMSwap.swap)
-        })
-
-        it("Should not swap if allowanc is too low",async function(){
-
-        })
-        it("Should swap properly",async function(){
-
-        })
-
-    })
-    
-    describe("Remove Liquidity",function(){
-
-    })
-})
+          describe("Remove Liquidity", function () {})
+      })
